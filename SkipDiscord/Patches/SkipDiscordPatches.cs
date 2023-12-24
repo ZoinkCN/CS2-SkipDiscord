494a9a58aa942bcbbb2140ec3fd4c924ac30f84f
ZoinkCN <zoinkcn@outlook.com>
2023-12-24 23:06:54 +0800

    添加项目文件。

---
+++ SkipDiscord/Patches/SkipDiscordPatches.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx.Logging;
+using Colossal.PSI.Discord;
+using HarmonyLib;
+
+namespace SkipDiscord.Patches
+{
+    [HarmonyPatch]
+    public class SkipDiscordPatches
+    {
+        private static ManualLogSource logger = Logger.CreateLogSource(MyPluginInfo.PLUGIN_NAME);
+        [HarmonyPatch(typeof(DiscordRichPresence), "Initialize")]
+        [HarmonyPrefix]
+        public static bool PreFix(ref bool ___m_IsInitialized)
+        {
+            ___m_IsInitialized = true;
+            return true;
+        }
+    }
+}
---
+++ SkipDiscord/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+#if BEPINEX_V6
+    using BepInEx.Unity.Mono;
+#endif
+
+
+namespace SkipDiscord
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            var harmony = new Harmony(MyPluginInfo.PLUGIN_NAME);
+            harmony.PatchAll();
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
+        }
+    }
+}
